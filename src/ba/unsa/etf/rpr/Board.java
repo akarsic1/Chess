7cff5dff67c7d39c3bccad691165d4c256757618
Amel <akarsic1@etf.unsa.ba>
2018-11-07 19:55:43 +0100

    Implementiran cloneabe u klasi chesspiece, kao i konstruktor u klasi board.

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -3,11 +3,41 @@ package ba.unsa.etf.rpr;
 import java.util.ArrayList;
 
 public class Board {
-    ArrayList<ChessPiece> Figures = new ArrayList<ChessPiece>();
+    ArrayList<ChessPiece> figures = new ArrayList<ChessPiece>();
+
+    public Board(){
+        figures.add(new King("E1",ChessPiece.color.WHITE));
+        figures.add(new King("E8",ChessPiece.color.BLACK));
+        figures.add(new Queen("D1",ChessPiece.color.WHITE));
+        figures.add(new Queen("D8",ChessPiece.color.BLACK));
+        figures.add(new Bishop("C1", ChessPiece.color.WHITE));
+        figures.add(new Bishop("F1", ChessPiece.color.WHITE));
+        figures.add(new Bishop("C8", ChessPiece.color.BLACK));
+        figures.add(new Bishop("F8", ChessPiece.color.BLACK));
+        figures.add(new Knight("B1",ChessPiece.color.WHITE));
+        figures.add(new Knight("G1",ChessPiece.color.WHITE));
+        figures.add(new Knight("B8",ChessPiece.color.BLACK));
+        figures.add(new Knight("G8",ChessPiece.color.BLACK));
+        figures.add(new Rook("A1",ChessPiece.color.WHITE));
+        figures.add(new Rook("H1",ChessPiece.color.WHITE));
+        figures.add(new Rook("A8",ChessPiece.color.BLACK));
+        figures.add(new Rook("H8",ChessPiece.color.BLACK));
+        for(char i = 'A'; i <='H'; i++){
+            String pozicija = i+"2";
+            figures.add(new Pawn(pozicija, ChessPiece.color.WHITE));
+        }
+        for(char i = 'A'; i <='H'; i++){
+            String pozicija = i+"7";
+            figures.add(new Pawn(pozicija, ChessPiece.color.BLACK));
+        }
+    }
 
     void move(Class type, ChessPiece.Color color, String position){}
+
     void move(String oldPosition, String newPosition){}
+
     boolean isCheck(ChessPiece.Color color){
+
         return true;
     }
 }
--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -1,13 +1,18 @@
 package ba.unsa.etf.rpr;
 
-public abstract class ChessPiece {
+public abstract class ChessPiece implements Cloneable{
 
     public enum Color{BLACK, WHITE}
     String position;
      public static Color color;
 
-    public static boolean checkIllegal(String pozicija){
-        if(pozicija == ""){return true;}
+     public Object clone()throws CloneNotSupportedException{
+            ChessPiece klon = (ChessPiece)super.clone();
+            return klon;
+     }
+
+    public boolean checkIllegal(String pozicija){
+        if((pozicija == "") || pozicija.equals(getPosition())){return true;}
         char slovo = pozicija.charAt(0);
         char broj = pozicija.charAt(1);
         if(pozicija.length() > 2 || pozicija.length()<= 0)return true;
